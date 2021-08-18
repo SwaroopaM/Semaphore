a3d75d93c61ef7929b874d406ed46fbcec1eff0d
MSPR-GPU <mspr@patterns-sdi.com>
2021-08-18 19:39:13 +0530

    fixed local issues

--- src/main/java/com/example/springpipelinedemo/model/User.java
+++ src/main/java/com/example/springpipelinedemo/model/User.java
@@ -30,7 +30,7 @@ public class User implements UserDetails {
     }
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     @Column(unique = true)
     private String email;
